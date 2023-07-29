b4d995b7123187694b2143d09807a98f83c15223
Aayush Rana <ayushrana38@gmail.com>
2023-07-29 23:38:21 +0530

    combinesReducers: using multiple reducers

---
+++ pureRedux/multipleReducers.js
@@ -0,0 +1,43 @@
+import {applyMiddleware, combineReducers, createStore} from "redux"
+import logger from "redux-logger";
+import thunk from 'redux-thunk';
+import axios from "axios"
+
+const inc  = "account/increment"; // stateName/actionType
+const dec  = "account/decrement";
+const incBonus  = "bonus/increment";
+
+// now if we want to use many reducer we can't write reducer here directly in store. 
+// Need to use combineReducers({ stateName: ReducerName }) 
+const store = createStore(
+  combineReducers({ // combineReducers is used to combine reducers
+    account: accountReducer, // account is state name and accountReducer is reducer name
+    bonus: bonusReducer
+  }), applyMiddleware(logger.default, thunk.default));
+
+// store.getState() - it is the combination of all state i.e global state 
+
+// accountReducer
+function accountReducer(state = { amount: 1 }, action){
+  switch(action.type){
+    case inc: return { amount : state.amount + action.payload }
+    default: return state;
+  }
+}
+
+// bonusReducer
+function bonusReducer(state = { points: 0 } ,action){
+  switch(action.type){
+    case inc: 
+      if(action.payload > 100){
+        return { points: state.points + 1}
+      }
+      return state
+    case incBonus: return { points: state.points + 1 }
+    default: return state
+  }
+}
+
+// dispatch se hum action bhejte ha { type: action.name , payload: value }
+store.dispatch( { type: inc, payload: 10 } );
+// agar dono reducer m same action type ka naam dedoge to dono reducer chalenge
